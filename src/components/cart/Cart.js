c9b68363f1bf7efc255d225dfd22f4f26d255599
Kevinr2 <kevinreve096@gmail.com>
2021-11-13 13:26:23 -0500

    mejorando el desing

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,13 +5,17 @@ import InfoCard from './InfoCard'
 
 export default function Cart() {
     return (
-        <View style={{ display: 'flex', alignItems: 'center', top: 20, height: 500 }}>
+        <View style={{ display: 'flex', alignItems: 'center', top: 20, }}>
             <InfoCard perfo={0} />
             <InfoCard perfo={20} />
-            <Text style={{ top: 30, width: '98%', backgroundColor: 'green', textAlign: 'center', height: 40, textAlignVertical: 'center' }}>total: 258.5</Text>
+            <View style={{ top: 30, width: '89%', height: 40, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Text >Total:</Text>
+                <Text style={{ fontSize: 20 }}>$25.5</Text>
+            </View>
+
             <View style={{ width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', top: 45 }}>
 
-                <View style={{ width: 100, height: 50, }}>
+                <View style={{ width: 100, height: 50 }}>
                     <Text>Person</Text>
                     <Text style={{ top: 3, textAlign: 'center', fontSize: 20 }}> -  1 +</Text>
                 </View>
