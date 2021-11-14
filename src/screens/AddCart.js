480088fdb0daf6e2e4ee6e57430ad5d54783c011
Kevinr2 <kevinreve096@gmail.com>
2021-11-14 09:20:44 -0500

    add, addCart.js

--- src/screens/AddCart.js
+++ src/screens/AddCart.js
@@ -3,11 +3,37 @@ import { View, Text, Image, Button, Pressable, } from 'react-native'
 
 
 export default function AddCart(props) {
-
-
-
     const { route } = props
     const { params } = route
+
+
+    const [amount, setAmount] = useState(0)
+    const [totals, setTotals] = useState(0.00)
+
+    const remove = () => {
+        if (amount > 0) {
+            setAmount(amount - 1)
+        }
+        Totalremove()
+    }
+    const Totalsum = () => {
+        if (amount === 0) {
+            setTotals(params.price)
+        } else if (amount > 0) {
+            setTotals(totals + params.price)
+        }
+    }
+    const Totalremove = () => {
+        if (amount > 0) {
+            setTotals(totals - params.price)
+        }
+    }
+    const sum = () => {
+        setAmount(amount + 1)
+        Totalsum()
+    }
+
+
 
     return (
         <View style={{ width: '100%', height: 200 }}>
@@ -15,13 +41,13 @@ export default function AddCart(props) {
             <Text style={{ fontSize: 15 }}>Description:</Text>
             <Text style={{ top: 10 }}>Lorem Ipsum is simply dummy text of theok a galley of type and scrambled it to make a type</Text>
             <Text style={{ top: 30, fontSize: 20, textAlign: 'center' }}>Price: {params.price}</Text>
-            <Text style={{ top: 50, textAlign: 'center', fontSize: 20 }}> total:0 </Text>
+            <Text style={{ top: 50, textAlign: 'center', fontSize: 20 }}> total: {(totals).toFixed(2)} </Text>
             <View style={{ top: 110, display: 'flex', flexDirection: 'row', justifyContent: 'space-around', }}>
-                <Pressable>
-                    <Text style={{ fontSize: 20 }}>-</Text>
+                <Pressable style={{ width: 50, height: 50, left: 30 }} onPress={() => remove()} >
+                    <Text style={{ fontSize: 20, }}>-</Text>
                 </Pressable>
-                <Text style={{ fontSize: 20 }}>0</Text>
-                <Pressable>
+                <Text style={{ fontSize: 20 }}>{amount}</Text>
+                <Pressable style={{ width: 50, height: 50 }} onPress={() => sum()} >
                     <Text style={{ fontSize: 20 }}>+</Text>
                 </Pressable>
             </View>
